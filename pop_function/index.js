84a56883611bd6feb38ef51b3465a6ad41fc53ee
Abdou <nahdi.abdo.2020@gmail.com>
2024-10-28 14:25:46 +0100

    removeStr

--- pop_function/index.js
+++ pop_function/index.js
@@ -1,10 +1,22 @@
-'use strict' ; 
+"use strict";
 
-const str = 'Ali' ; 
-function removeStr(str , char , index){
-
+const str = "Abdou";
+function removeStr(str, char, index) {
+  let arr = [...str];
+  let check = false;
+  for (const element of arr) {
+    if (element == char) {
+      check = true;
+    }
+  }
+  if (arr[index] !== char && check) {
+    console.log(`${char} dosen’t exist at this position`);
+  } else if (!check) {
+    console.log(`${char} dosen’t exist in this string`);
+  } else {
+    arr = arr.join().replaceAll(",", "");
+    console.log(arr.replaceAll(char, ""));
+  }
 }
 
-removeStr(str,'A',0)
-
-
+removeStr(str, "b", 1);
