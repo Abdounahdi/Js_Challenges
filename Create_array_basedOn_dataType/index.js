100cb0bae8f603e66dd7dba2387a12fea1c6fe2d
Abdou <nahdi.abdo.2020@gmail.com>
2024-10-28 08:51:16 +0100

    finished unzip function

--- Create_array_basedOn_dataType/index.js
+++ Create_array_basedOn_dataType/index.js
@@ -1,1 +1,24 @@
 'use strict' ; 
+const zipedArr = [['a', 1, true], ['b', 2]]; 
+
+function unzip(arr){
+  const numberArr = []
+  const stirngArr = []
+  const booleanArr = []
+  for (const array of zipedArr){
+    for (const element of array){
+      if (typeof (element) == 'number'){
+        numberArr.push(element) ; 
+      }else if (typeof (element) == 'string') {
+        stirngArr.push(element) ; 
+      }else if (typeof (element) == 'boolean') {
+        booleanArr.push(element) ; 
+      }
+    }
+  }
+
+  return [stirngArr , numberArr , booleanArr]
+}
+
+
+console.log(unzip(zipedArr))
