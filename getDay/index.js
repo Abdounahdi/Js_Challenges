cb23e496675f3bd1e896beae484c64e134fec2dc
Abdou <nahdi.abdo.2020@gmail.com>
2024-10-29 08:19:13 +0100

    added tests

--- getDay/index.js
+++ getDay/index.js
@@ -1,10 +1,20 @@
-'use strict'; 
+"use strict";
 
-function getDay(str){
-  const weekDays = ['sunday', 'monday' , 'tuesday','wednesday' , 'thirsday' , 'friday', 'saturday']
-  const dateDay =((new Date(`${str.slice(3,5)}/${str.slice(0,2)}/${str.slice(6)}`)).getDay() );
-  if (dateDay !== NaN)console.log(weekDays[dateDay]);
+function getDay(str) {
+  const weekDays = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thirsday",
+    "friday",
+    "saturday",
+  ];
+  const dateDay = new Date(
+    `${str.slice(3, 5)}/${str.slice(0, 2)}/${str.slice(6)}`
+  ).getDay();
+  if (dateDay !== NaN) console.log(weekDays[dateDay]);
 }
 
-
-
+getDay("07/11/2000");
+getDay("11/06/2017");
